7ef49df3cefae6230f6696dee4cc8db5721f84c1
MiguelTAR <mtarazona4489@conestogac.on.ca>
2022-12-08 09:53:00 -0500

    creating the screen and the turtle pen icon

--- Final Project.py
+++ Final Project.py
@@ -1,3 +1,25 @@
 #Miguel Tarazona 
 #Final Project
-#THe purpose: To create paint
+#The purpose: To create paint
+#Professor: 2022-12-07
+
+#importing the library turtle and random 
+import turtle 
+from turtle import Turtle, Screen
+import random
+
+#The window size, the colour of the screen and the title 
+canvas = turtle.Screen()
+canvas.title(" Miguel's Paint ")
+canvas.bgcolor("snow")
+canvas.setup(width = 800, height = 800)
+
+#Creating the icon of the pen which will be a turtle 
+turtle_icon = Turtle("turtle")
+turtle_icon.width(3)
+turtle_icon.speed(-1)
+
+#All of the 8 colours used in this program
+colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'black']
+
+
